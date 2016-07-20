f645e5ed6abdda6d630890fd5b6a540e7cd5e415
Daniel C <bitclaw@gmail.com>
2016-07-20 15:12:55 -0500

    Update composer dependencies and algorithm.

--- tests/PHPCode/Test/AlgorithmTest.php
+++ tests/PHPCode/Test/AlgorithmTest.php
@@ -2,10 +2,18 @@
 
 namespace PHPCode\Test;
 
+use PHPCode;
+
 class AlgorithmTest extends \PHPUnit_Framework_TestCase{
 
-    public function testMinimumViableTest(){
+    public function testMinimumViableTest() {
         $this->assertTrue(true, "true didn't end up being false!!!");
+    }
+
+    public function testAddAllNumbers() {
+        $algo = new PHPCode\Algorithm();
+        $result = $algo->addAllNumbers(2);
+        $this->assertCount(2,$result);
     }
 
 }
